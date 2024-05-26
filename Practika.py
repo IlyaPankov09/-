01777d43abc90314f0517fe9a89ced0e2885da01
Илья <pankov_ie@mail.ru>
2024-05-26 17:17:16 +0300

    Add files via upload

---
+++ Practika.py
@@ -0,0 +1,20 @@
+print('"hello world"')
+print(len('hello world'))
+first= 5
+second=8
+summa=first+second
+diff=first-second
+print(summa,diff)
+first=4
+second=6
+third=8
+mean=((first+second+third)//3)
+print(mean)
+first_str = 'вторник'
+second_str = 'понедельник'
+print(second_str,',',' ',first_str)
+a=2
+b=4
+c=6
+f=(a*b)+(a*c)
+print((f**3)//2)
