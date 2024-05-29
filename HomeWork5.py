3c22916b21bf7d91f776b32d295dd56c0422623f
Илья <pankov_ie@mail.ru>
2024-05-29 16:09:07 +0300

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,20 @@
+immutable_var= (1,'world',True)
+print(immutable_var)
+#immutable_var[0] = 200 # элемент кортежа нельзя изменить
+mutable_list=[1, 5, 'apple', True]
+print(mutable_list)
+mutable_list[2]= 'peach'
+print(mutable_list)
+#mutable_list.append('apple')
+#print(mutable_list)
+#mutable_list.extend(['dog'])
+#print(mutable_list)
+#print(mutable_list[5])
+#mutable_list.remove(5)
+#print(mutable_list)
+#print('apple' in mutable_list)
+#print('peach'in mutable_list)
+#print(mutable_list.__sizeof__())
+#print(immutable_var.__sizeof__())
+
+
