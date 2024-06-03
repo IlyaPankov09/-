5ad46059935a54476f71e4c98609f53f0f650bb8
Илья <pankov_ie@mail.ru>
2024-06-03 10:48:03 +0300

    Add files via upload

---
+++ Modul_2_2.py
@@ -0,0 +1,9 @@
+first = 5
+second = 7
+third = 9
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
