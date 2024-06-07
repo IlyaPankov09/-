1c0b67717892bbb07501d082c196fc481391d822
Илья <pankov_ie@mail.ru>
2024-06-07 12:59:30 +0300

    Add files via upload

--- Modul_2_last.py
+++ Modul_2_last.py
@@ -1,18 +1,16 @@
 first_field = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 result = []
 import random
-def number():                         #функция выбирает случайное число из первого поля
-    n = random.choice(first_field)
-    return n
-print(number())
+n = random.choice(first_field)
+print(n)
 sum = 0
-for i in range(1, 21):
-    for j in range(1, 21):
+for i in range(1, n):
+    # count = 0
+    for j in range(i+1, n):
         sum = i + j
-        #print(sum)    #вроде складывает
-        if i  + j == number and number % sum == 0:
-            result.append(i)
-            continue
-print(result)
+        if n % sum == 0:
+            # count += 1
+            result.append(f'{i}{j}')
+print(''.join(result))
 
 
