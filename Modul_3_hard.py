76b7db9033560bc9290801c3b86ee4e5c1c7aa61
Илья <pankov_ie@mail.ru>
2024-06-17 16:11:55 +0300

    Add files via upload

--- Modul_3_hard.py
+++ Modul_3_hard.py
@@ -4,17 +4,17 @@ data_structure = [[1, 2, 3],{'a':4, 'b':5},(6,{'cube':7,'drum':8}),"Hello",((),[
 #     print(type(i))
 def calculate_structure_sum(data_structure):
     summa = 0
-    for i in data_structure:
-        if isinstance(i, (list, tuple, set)):
+    if isinstance(data_structure, list) or isinstance(data_structure, tuple) or isinstance(data_structure,set):
+        for i in data_structure:
             summa += calculate_structure_sum(i)
-        elif isinstance(i, str):
+    elif isinstance(data_structure, str):
             summa += len(data_structure)
-        elif isinstance(i, (int, float)):
-            summa += i
-        elif isinstance(i, dict):
-           for key, value in i.items():
-                summa += calculate_structure_sum(key)
-                summa += calculate_structure_sum(value)
+    elif isinstance(data_structure, int) or isinstance(data_structure, float):
+            summa += data_structure
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            summa += calculate_structure_sum(key)
+            summa += calculate_structure_sum(value)
     return summa
 print(calculate_structure_sum(data_structure))
 
