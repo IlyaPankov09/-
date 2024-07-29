39b1a15d939104d362f991b70a2fb15c0541ce54
Илья <pankov_ie@mail.ru>
2024-07-29 17:17:50 +0300

    Add files via upload

--- modul 7_3.py
+++ modul 7_3.py
@@ -1,21 +1,18 @@
 import io
 from pprint import pprint
 class WordsFinder:
-    def __init__(self, file_names):
-        self.file_names = file_names
+    def __init__(self, *file_names):
+        self.file_names = list(file_names)
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
             with open(file_name, 'r', encoding='utf-8') as file:
-                words = []
-                for line in file:
-                    line = line.lower()
-                    punctuation = [',', '.', '=', '!', '?', ';', ':']
-                    for punct in punctuation:
-                        line = line.replace(punct, '')
-                    line = line.replace('-','')
-                    words.extend(line.split())
-                all_words[i] = words
+                info = file.read().lower()
+                punctuation = [',', '.', '=', '!', '?', ';', ':', ' - ']
+                for punct in punctuation:
+                    info = info.replace(punct, '')
+                words = info.split()
+                all_words[file_name] = words
         return all_words
     def find(self, word):
         places = {}
