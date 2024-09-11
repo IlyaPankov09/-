2af6055c89e60a1233df23b0be9146ecf3432a20
Илья <pankov_ie@mail.ru>
2024-09-11 21:25:36 +0300

    Add files via upload

--- modul_10_2.py
+++ modul_10_2.py
@@ -4,21 +4,21 @@ from time import sleep
 class Knight(Thread):
 
     def __init__(self, name = str, power = int):
+        super().__init__()
         self.name = name
         self.power = power
-        super().__init__()
 
     def run(self):
         print(f'{self.name}, на нас напали!')
         life = 100
         days = 0
         while life > 0:
-            sleep(1)
             days += 1
             life -= self.power
-            if life < 0:
+            if life < self.power:
                 life = 0
             print(f'{self.name} сражается {days} суток, осталось {life} воинов врага.')
+            sleep(1)
         print(f'{self.name} одержал победу спустя {days} дней(дня)!')
 def main():
     first_knight = Knight('Sir Lancelot', 10)
